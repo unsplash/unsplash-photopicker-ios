e82832d3fcdb554cb1b6e6dd4f56b2693096fbe1
Nicolas Bichon <bichon.nicolas@gmail.com>
2018-10-08 16:40:43 -0400

    Create Cocoa Touch Framework

---
+++ UnsplashPhotoPicker/UnsplashPhotoPicker/UnsplashPhotoPicker.h
@@ -0,0 +1,19 @@
+//
+//  UnsplashPhotoPicker.h
+//  UnsplashPhotoPicker
+//
+//  Created by Bichon, Nicolas on 2018-10-08.
+//  Copyright © 2018 Unsplash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnsplashPhotoPicker.
+FOUNDATION_EXPORT double UnsplashPhotoPickerVersionNumber;
+
+//! Project version string for UnsplashPhotoPicker.
+FOUNDATION_EXPORT const unsigned char UnsplashPhotoPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnsplashPhotoPicker/PublicHeader.h>
+
+
