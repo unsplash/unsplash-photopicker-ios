610d4f4153986263c27308f3d6a34a725c853e7d
Nicolas Bichon <bichon.nicolas@gmail.com>
2018-10-08 16:42:17 -0400

    Add Example target

---
+++ UnsplashPhotoPicker/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Bichon, Nicolas on 2018-10-08.
+//  Copyright © 2018 Unsplash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
